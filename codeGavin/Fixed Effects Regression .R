84b9e23888f5a68593c476e20251655dc335d636
Gavin Scoville <gavin.scoville@gmail.com>
2024-05-05 21:17:52 -0700

    I think line 73-76 contians the three most general methods for detrending, but I would like to confirm this

--- codeGavin/Fixed Effects Regression .R
+++ codeGavin/Fixed Effects Regression .R
@@ -15,6 +15,9 @@ rwls <- readRDS("dataVault/rwl21c.rds")
 some funcitons I will be calling on:
 _______________________________________________________________________________'
 
+
+cutoff<- 2000 #year we start analyzing at 
+
 #to identify the columns that are trees, inorder to pivot_longer
 tree_columns <- function(df) {
   if (!is.data.frame(df)) {  # Trying to use more Validation 
@@ -39,8 +42,7 @@ extract_plm_values <- function(model) {
 find_recent_trend_plm <- function(df) {
   rwi <- df
   rwi$year <- as.numeric(rownames(rwi))
-  recent_rwi <- rwi[as.numeric(rownames(rwi)) > 2000, ]
-  
+  recent_rwi <- rwi[as.numeric(rownames(rwi)) > cutoff, ]
   if (nrow(recent_rwi) <= 2) {
     print("Recent data is near empty")
     return(c(NA,NA,NA))
@@ -53,10 +55,6 @@ find_recent_trend_plm <- function(df) {
     )
     p_recent_rwi <- pdata.frame(long_recent_rwi, index = "tree")
     
-    if (nrow(p_recent_rwi) == 0) {
-      print("pdata.frame is empty")
-      return(c(NA,NA,NA))
-    } else {
       model_fe <- plm(std ~ year, data = p_recent_rwi, model = "within")
       
       if (all(model_fe$coefficients == 0)) {
@@ -68,38 +66,60 @@ find_recent_trend_plm <- function(df) {
       }
     }
   }
-}
 #to wrap it all together into something you can vapply:
 climate_effects<-function(df){
-  rwi_ModNegExp<- detrend(rwl = df, method = "ModNegExp")#makes 3 different estimate (to show robustness)
+  # here i provide a stop for if there arn't enough years in the 21st century 
+  recent_years <- df[as.numeric(rownames(df)) > cutoff, ]
+  if (nrow(recent_years) <= 2) {
+    print("Recent data is near empty")
+    return(c(NA,NA,NA,NA,NA,NA,NA,NA,NA))
+  } else {
+   #makes 3 different estimate (to show robustness)
+  rwi_ModNegExp<- detrend(rwl = df, method = "ModNegExp")
   rwi_Mean<- detrend(rwl = df, method = "Mean")
   rwi_AgeDepSpline <- detrend(rwl = df, method = "AgeDepSpline")
   rwis<-list(ModNegExp = rwi_ModNegExp, Mean = rwi_Mean, AgeDepSpline = rwi_AgeDepSpline)
+
   trends<-vapply(rwis, find_recent_trend_plm, FUN.VALUE = numeric(3),USE.NAMES = TRUE)#applying plm to all three
   flattened_trends <- as.vector(trends)
   return(flattened_trends)
+  }
 }
-
-df<-detrend(rwl= rwls[[1]],method = "Mean")
+#testing functions:
+df<-detrend(rwl= rwls[[10]], method = "Mean")
 find_recent_trend_plm(df)
-
+climate_effects(rwls[[1]])
+climate_effects(rwls[[341]])
+rwls[[10]]
 '______________________________________________________________________________
 Applying the functions to the data:
 _______________________________________________________________________________'
+stand_names <- rownames(rwls)
 
-list_of_estimates<-data.frame()
-for( i in 1:10){#for dubugging purposes 
+list_of_estimates <- data.frame(
+  ModNegExp = numeric(),
+  SE_ModNegExp = numeric(),
+  P_ModNegExp = numeric(),
+  Mean = numeric(),
+  SE_Mean = numeric(),
+  P_Mean = numeric(),
+  AgeDepSpline = numeric(),
+  SE_AgeDepSpline = numeric(),
+  P_AgeDepSpline = numeric()
+)
+
+for( i in 1:length(rwls)){#for dubugging purposes 
   df<-rwls[[i]]
   print(paste("stand #", i, sep=""))
   stand_climate_effects<-climate_effects(df)
-  list_of_estimates[i,]<-t(stand_climate_effects)
+  list_of_estimates[i,]<-c(stand_climate_effects)
 }
-
-
+#30 minutes 
+summary(list_of_estimates)
 #for real use purposes 
 list_of_estimates<-vapply(rwls, climate_effects, FUN.VALUE = numeric(9), USE.NAMES = TRUE)
 
-colnames(table_of_estimates) <- c("Stand", 
+colnames(table_of_estimates) <- c( 
                                   "ModNegExp",
                                   "SE_ModNegExp",
                                   "P_ModNegExp",
